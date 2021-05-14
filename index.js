6d1dcf224f012ede72802758231d74b1032f8172
Colin Donaghy <>
2021-05-14 10:13:32 +0000

    Added stuff

--- index.js
+++ index.js
@@ -23,14 +23,55 @@ app.post('/api/downloader/', (req, res) => {
 
   try {
     ytdl(url)
+    
     .pipe(
       fs.createWriteStream(`./public/video${rand}.mp4`)
     );
-    res.sendFile(`video${rand}.mp4`, { root: './public'})
+    res.redirect('/api/video?name=video'+rand+'.mp4')
   } catch(err) {
     res.send({error: err})
   }
+})
 
+app.get('/api/video', (req, res) => {
+  const videoName = req.query.name
+  const videoPath = `./public/${videoName}`
+  if(!videoName) {
+    res.send({error: 'enter name'})
+    return
+  }
+  try {
+    const videoStat = fs.statSync(videoPath);
+    const fileSize = videoStat.size;
+    const videoRange = req.headers.range;
+    if (videoRange) {
+        const parts = videoRange.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
+        const chunksize = (end-start) + 1;
+        const file = fs.createReadStream(videoPath, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(videoPath).pipe(res);
+    }
+    // res.sendFile(videoPath, {root : './'})
+  } catch(err) {
+    res.send({error :'That file doesnt exist!'})
+  }
 })
 
 app.listen(port, () => {
